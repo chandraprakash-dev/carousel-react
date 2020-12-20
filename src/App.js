22b17c81e2cddc80cea83c9cdc46a68f8a1ceeea
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-12-20 18:25:13 +0530

    Write functionality to move products in carousel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {useState} from 'react';
 import productsData from './productsData.json';
 import Product from "./Components/Product";
 
 function App() {
-  const products = productsData.map((productData, id) => <Product key={id} data={productData}/>);
+  const [index, setIndex] = useState(1);
+
+  function moveSlide(n) {
+    let newIndex = index + n;
+    console.log(newIndex)
+    if(newIndex < 1) newIndex = productsData.length;
+    else if(newIndex >= productsData.length) newIndex = 1;
+    setIndex(newIndex);
+  }
 
   return (
     <div className="App">
@@ -10,9 +19,12 @@ function App() {
       </header>
       <main>
         <div className="slide-carousel">
-          {products}
-          <a id="prev">&#10094;</a>
-          <a id="next">&#10095;</a>
+          {productsData.map((productData, id) => <Product key={id}
+                                                          data={productData}
+                                                          index={index}
+          />)}
+          <span onClick={() => moveSlide(-1)} id="prev">&#10094;</span>
+          <span onClick={() => moveSlide(1)} id="next">&#10095;</span>
         </div>
       </main>
     </div>
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 function Product(props) {
+  const show = props.index === props.data.id ? 'show' : 'slide';
   return(
-    <div className="product slide fade">
+    <div className={"product fade " + show}>
       <div className="image"><img src={props.data.image} alt={props.data.name} /></div>
       <p>{props.data.name}</p>
       <p>{props.data.price}</p>
