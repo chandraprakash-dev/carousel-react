bcad24279fa7b06c3b1148d418411fc287adc062
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-12-20 15:00:40 +0530

    add image to product card and do basic styling

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,6 +3,7 @@ import React from "react";
 function Product(props) {
   return(
     <div className="product">
+      <div className="image"><img src={props.data.image} alt={props.data.name} /></div>
       <p>{props.data.name}</p>
       <p>{props.data.price}</p>
       <p>{props.data.category}</p>
