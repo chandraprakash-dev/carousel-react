66b24a484824c22a262c78a628a197576ca3771c
Chandra Prakash <pr.chandraprakash@gmail.com>
2020-12-20 15:32:41 +0530

    Add sliding carousel div with arrow keys

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import productsData from './productsData.json';
 import Product from "./Components/Product";
 
 function App() {
-  const products = productsData.map(productData => <Product data={productData}/>);
+  const products = productsData.map((productData, id) => <Product key={id} data={productData}/>);
 
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <main>
-        {products}
+        <div className="slide-carousel">
+          {products}
+          <a id="prev">&#10094;</a>
+          <a id="next">&#10095;</a>
+        </div>
       </main>
     </div>
   );
